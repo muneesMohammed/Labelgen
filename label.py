006dcd4e5628246739eb0169a116ad030802aab3
unknown <marhaba.munees@gmail.com>
2024-08-05 02:50:17 +0400

    m

--- label.py
+++ label.py
@@ -82,10 +82,10 @@ class LabelGeneratorApp:
         width, height = letter
 
         labels_per_page = 1  # Number of labels per page
-        label_height = 6 * inch
-        label_width = 4 * inch
-        x_margin = 0.5 * inch
-        y_margin = height - 0.5 * inch
+        label_height = 11 * inch
+        label_width = 7.3 * inch
+        x_margin = 1 * inch
+        y_margin = height - 1 * inch
 
         for i in range(Nooflabel):
             x_position = x_margin
@@ -94,45 +94,45 @@ class LabelGeneratorApp:
             # Draw a line
             c.setLineWidth(1)
             c.setStrokeColor(colors.black)
-            c.line(x_position, y_position - 1.6 * inch , x_position + label_width, y_position - 1.6 * inch)
-            c.line(x_position, y_position - 2.3 * inch , x_position + label_width, y_position - 2.3 * inch)
-            c.line(x_position, y_position - 3.0 * inch , x_position + label_width, y_position - 3.0 * inch)
+            c.line(x_position, y_position - 2.9 * inch , x_position + label_width, y_position - 2.9 * inch)
+            c.line(x_position, y_position - 4.16* inch , x_position + label_width, y_position - 4.16* inch)
+            c.line(x_position, y_position - 5.3 * inch , x_position + label_width, y_position - 5.3 * inch)
+            c.line(x_position, y_position - 6.5 * inch , x_position + label_width, y_position - 6.5 * inch)
+            c.line( y_position - 5.65 * inch ,x_position +1 * inch,  y_position - 5.65 * inch,x_position + label_width- 2.45 * inch)
             # Insert hyphen after the first 3 digits
             HifenAirwaybillno = airwaybillno[:3] + '-' + airwaybillno[3:]
             # Draw the text
             num = i+1
             padded_num = str(num).rjust(5, '0')
             # Draw the text
-            c.setFont("CustomFont", 60)
+            c.setFont("CustomFont", 110)
             c.setFillColor(colors.black)
-            c.drawString(x_position + 0.15 * inch, y_position - 1.25 * inch, f"{airwaybillno+padded_num}")
+            c.drawString(x_position + 0.15 * inch, y_position - 2.25 * inch, f"{airwaybillno+padded_num}")
 
-            c.setFont("Helvetica", 12)
-            c.setFillColor(colors.black)
 
             # Set font and color for the title
-            c.setFont("Helvetica", 12)
+            c.setFont("Helvetica-Bold", 25)
             c.setFillColor(colors.black)
-            c.drawString(x_position + 1.25 * inch, y_position - 1.5 * inch, f"{airwaybillno+padded_num}")
-            c.drawString(x_position + 0.25 * inch, y_position - 1.8 * inch, f"Air Waybill No.")
-            c.setFont("Helvetica-Bold", 32)
+            c.drawString(x_position + 1.8 * inch, y_position - 2.7 * inch, f"{airwaybillno+padded_num}")
+            c.drawString(x_position + 0.15 * inch, y_position - 3.2 * inch, f"Air Waybill No.")
+            c.setFont("Helvetica-Bold", 67)
             c.setFillColor(colors.black)
-            c.drawString(x_position + 0.50 * inch, y_position - 2.2 * inch, f"{HifenAirwaybillno}")
-            c.setFont("Helvetica", 12)
+            c.drawString(x_position + 0.50 * inch, y_position - 4 * inch, f"{HifenAirwaybillno}")
+            c.setFont("Helvetica-Bold", 25)
             c.setFillColor(colors.black)
             
-            c.drawString(x_position + 0.25 * inch, y_position - 2.5 * inch, f"Destination")
-            c.drawString(x_position + 0.25 * inch, y_position - 2.8 * inch, f"{destination}")
-            c.drawString(x_position + 2.0 * inch, y_position - 2.5 * inch, f"Total No. Of Pcs")
-            c.drawString(x_position + 2.25 * inch, y_position - 2.8 * inch, f"{noofpieces}")
-            c.drawString(x_position + 0.25 * inch, y_position - 5.0 * inch, f"Product Name")
-            c.drawString(x_position + 0.25 * inch, y_position - 5.5 * inch, f"{productname}")
-            c.drawString(x_position + 0.25 * inch, y_position - 6.0 * inch, f"Weight")
-            c.drawString(x_position + 0.25 * inch, y_position - 6.5 * inch, f"{weight}")
+            c.drawString(x_position + 0.25 * inch, y_position - 4.5 * inch, f"Destination")
+            c.drawString(x_position + 0.35 * inch, y_position - 5.1 * inch, f"{destination}")
+            c.drawString(x_position + 3.5 * inch, y_position - 4.5 * inch, f"Total No. Of Pcs")
+            c.drawString(x_position + 3.75 * inch, y_position - 5.1 * inch, f"{noofpieces}")
+            c.drawString(x_position + 0.25 * inch, y_position - 5.7 * inch, f"Product Name")
+            c.drawString(x_position + 0.35 * inch, y_position - 6.2 * inch, f"{productname}")
+            c.drawString(x_position + 3.5 * inch, y_position - 5.7 * inch, f"Weight")
+            c.drawString(x_position + 3.75 * inch, y_position - 6.2 * inch, f"{weight}")
             c.drawString(x_position + 0.25 * inch, y_position - 7.0 * inch, f"Handling in for")
-            c.drawString(x_position + 0.25 * inch, y_position - 7.5 * inch, f"{handling}")
-            c.drawString(x_position + 0.25 * inch, y_position - 8.0 * inch, f"HAWB No.")
-            c.drawString(x_position + 0.25 * inch, y_position - 8.5 * inch, f"{hawbno}")
+            c.drawString(x_position + 0.35 * inch, y_position - 7.5 * inch, f"{handling}")
+            c.drawString(x_position + 3.5 * inch, y_position - 7.0 * inch, f"HAWB No.")
+            c.drawString(x_position + 3.75 * inch, y_position - 7.5 * inch, f"{hawbno}")
 
             if (i + 1) % labels_per_page == 0 and i != Nooflabel - 1:
                 c.showPage()  # Create a new page for the next labels
